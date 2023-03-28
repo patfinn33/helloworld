46594775b22d55f86016ba97cfb99ff6a2253dc5
patfinn33 <116234486+patfinn33@users.noreply.github.com>
2023-03-27 22:11:31 -0400

    added BTC total counts
    playing locally

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import locale
 
 print('hello World!')
 
@@ -8,3 +9,11 @@ response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 data = response.json()
 
 print(f"Bitcoin price: {data['bpi']['USD']['rate']}")
+
+response = requests.get("https://api.blockchain.info/charts/total-bitcoins?format=json")
+data = response.json()
+
+total_bitcoins = data["values"][-1]["y"]
+formatted_total_bitcoins = locale.format_string("%d", total_bitcoins, grouping=True)
+
+print(f"Total number of Bitcoins in circulation: {formatted_total_bitcoins}")
