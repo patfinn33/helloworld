c8cbda8ce5b3020ed1f3e191779d8cd746ed295d
patfinn33 <116234486+patfinn33@users.noreply.github.com>
2023-03-27 21:56:57 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+import requests
+
 print('hello World!')
+
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+data = response.json()
+
+print(f"Bitcoin price: {data['bpi']['USD']['rate']}")
